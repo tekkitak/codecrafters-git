039bbc83cee7c1cce6582d44d26016a5fcdcf2de
tekkitak <tekkitakcz@gmail.com>
2022-05-15 22:25:46 +0200

    Stage 4

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+from ast import Try
 from distutils.command.build import build
 from hashlib import sha1
 import sys
 import os
 import zlib
+
 
 def ReadZlib(filepath):
     with open(filepath, "rb") as compressed:
@@ -27,16 +29,26 @@ def main():
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
 
+    elif command == "debug-sha":
+        if len(sys.argv) > 1:
+            for sha in sys.argv[2:]:
+                filepath = f".git/objects/{sha[:2]}/{sha[2:]}"
+                if not os.path.exists(filepath):
+                    print(f"Object '{sha}' does not exist")
+                    RuntimeError(f"Object '{sha}' does not exist")
+                else:
+                    print(f"Object '{sha}' exists")
+                    print(f"Hash:\n {ReadZlib(filepath)}", "\n\n")
+
     elif command == "cat-file":
         if sys.argv[2] == "-p" and len(sys.argv) > 2:
             filepath = f".git/objects/{sys.argv[3][:2]}/{sys.argv[3][2:]}"
             if not os.path.exists(filepath):
-                print(f"Blob '{sys.argv[3]}' does not exist")
-                RuntimeError(f"Blob '{sys.argv[3]}' does not exist")
+                print(f"Object '{sys.argv[3]}' does not exist")
+                RuntimeError(f"Object '{sys.argv[3]}' does not exist")
             else:
                 decompressed = ReadZlib(filepath)
                 parts = decompressed.split(b"\x00")
-                #print(filepath, "\n", decompressed, "\n\n")
                 blob_type = parts[0].split(b" ")[0]
                 if blob_type == b"tree":
                     data = [str(item)[2:-1] for item in decompressed.split(b"\x00")[1].split(b" ")]
@@ -51,6 +63,36 @@ def main():
         else: 
             RuntimeError(f"Paramater flag '{sys.argv[2]}' doesnt exist for '{command}'")
     
+    elif command == "ls-tree":
+        if sys.argv[2] == "--name-only" and len(sys.argv) > 2:
+            filepath = f".git/objects/{sys.argv[3][:2]}/{sys.argv[3][2:]}"
+            if not os.path.exists(filepath):
+                print(f"Object '{sys.argv[3]}' does not exist")
+                RuntimeError(f"Object '{sys.argv[3]}' does not exist")
+            else:
+                decompressed = ReadZlib(filepath)
+                parts = decompressed.split(b"\x00")
+                blob_type = parts[0].split(b" ")[0]
+                if blob_type != b"tree":
+                    print(f"Object '{sys.argv[3]}' is not a tree")
+                    RuntimeError(f"Object '{sys.argv[3]}' is not a tree")
+
+                data = decompressed.split(b"\x00", 1)[1]
+                parsed = []
+                while True:
+                    try:
+                        split = data.index(b"\0")
+                    except ValueError:
+                        break
+                    tmp = data[:split].split(b" ")
+                    tmp.append(''.join(format(x, '02x') for x in data[split:split+20]))
+                    parsed.append(tmp)
+                    data = data[split+21:]
+                for row in parsed: print(row[1].decode(), end="\n")
+        else:
+            print(f"Paramater flag '{sys.argv[2]}' doesnt exist for '{command}'")
+            RuntimeError(f"Paramater flag '{sys.argv[2]}' doesnt exist for '{command}'")
+
     elif command == "hash-object":
         if sys.argv[2][0] == "-" and len(sys.argv) > 2:
             if sys.argv[2][1:] == "w":
