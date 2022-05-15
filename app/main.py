9f60172a981b07ee50b2140109bd59f7364ef2b3
tekkitak <tekkitakcz@gmail.com>
2022-05-15 16:15:35 +0200

    Stage 2

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
+from distutils.command.build import build
 import sys
 import os
+import zlib
 
-
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
+def ReadZlib(filepath):
+    with open(filepath, "rb") as compressed:
+        data = compressed.read()
+        d = zlib.decompressobj()
+        return d.decompress(data)
     
+def main():
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -16,8 +18,24 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+
+    elif command == "cat-file":
+        if sys.argv[2] == "-p" and len(sys.argv) > 2:
+            filepath = f".git/objects/{sys.argv[3][:2]}/{sys.argv[3][2:]}"
+            if not os.path.exists(filepath):
+                print(f"Blob '{sys.argv[3]}' does not exist")
+                RuntimeError(f"Blob '{sys.argv[3]}' does not exist")
+            else:
+                decompressed = ReadZlib(filepath)
+                data = [str(item)[2:-1] for item in decompressed.split(b"\x00")[1].split(b" ")]
+                next_blob = ''.join(format(x, '02x') for x in decompressed.split(b"\x00")[2])
+                print(f"{data[0]} blob {next_blob}\t{data[1]}")                
+                            
+        else: 
+            RuntimeError(f"Paramater flag '{sys.argv[2]}' doesnt exist for '{command}'")
+
     else:
-        raise RuntimeError(f"Unknown command #{command}")
+        raise RuntimeError(f"Unknown command '{command}'")
 
 
 if __name__ == "__main__":
